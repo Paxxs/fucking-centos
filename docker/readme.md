4ff2d2de362ac1767421f82501baae52d51c041b
Paxxs <10364775+Paxxs@users.noreply.github.com>
2021-08-22 19:10:21 +0800

    init

---
+++ docker/readme.md
@@ -0,0 +1,4 @@
+```shell
+docker build --rm -t local/centos:7-ssh docker/7/ssh/
+docker build --rm -t local/centos:7-ssh-go-1.17 docker/7/go/
+```
--- main.go
+++ main.go
@@ -1,28 +1,97 @@
 package main
 
 import (
+	"bufio"
 	"embed"
 	"fmt"
 	"io/ioutil"
+	"os/exec"
 	"regexp"
 	"strconv"
 )
 
 // cspell:words Centos
-// go:embed rpm
+
+//go:embed rpm
 
 var f embed.FS
 
 func main() {
-	fmt.Println("😀当前系统为", getCentosVersion("major"), "版本")
+	version := getCentosVersion("major")
+	fmt.Println("😀当前系统为", version, "版本")
+
+	// 查看嵌入的资源
+	// dirEntries, _ := f.ReadDir("rpm")
+	// for _, de := range dirEntries {
+	// 	fmt.Println(de.Name(), de.IsDir())
+	// }
+
+	switch version {
+	case 8:
+		fmt.Println("📦 install shell")
+
+		fish, err := f.ReadFile(`rpm/8/fish-3.3.1-1.4.x86_64.rpm`)
+		checkError(err)
+		// todo: 目录问题
+		err = ioutil.WriteFile(`/tmp/1.rpm`, fish, 0777)
+		checkError(err)
+		err = runCommand(`/usr/bin/yum`, `install`, "-y", `/tmp/1.rpm`)
+		checkError(err)
+
+		fmt.Println("📦 install tmux")
+		tmux, err := f.ReadFile(`rpm/8/tmux-2.6-1.11.x86_64.rpm`)
+		checkError(err)
+		err = ioutil.WriteFile(`/tmp/2.rpm`, tmux, 0777)
+		checkError(err)
+		err = runCommand(`/usr/bin/yum`, `install`, "-y", `/tmp/2.rpm`)
+		checkError(err)
+	case 7:
+		fmt.Println("📦 install shell")
+
+		fish, err := f.ReadFile(`rpm/7/fish-3.3.1-1.1.x86_64.rpm`)
+		checkError(err)
+		// todo: 目录问题
+		err = ioutil.WriteFile(`/tmp/1.rpm`, fish, 0777)
+		checkError(err)
+		err = runCommand(`/usr/bin/yum`, `install`, "-y", `/tmp/1.rpm`)
+		checkError(err)
+
+		fmt.Println("📦 install tmux")
+		tmux, err := f.ReadFile(`rpm/7/tmux-2.9a-4.4.x86_64.rpm`)
+		checkError(err)
+		err = ioutil.WriteFile(`/tmp/2.rpm`, tmux, 0777)
+		checkError(err)
+		err = runCommand(`/usr/bin/yum`, `install`, "-y", `/tmp/2.rpm`)
+		checkError(err)
+	case 6:
+		fmt.Println("📦 install shell")
+
+		fish, err := f.ReadFile(`rpm/6/fish-3.1.2+1603.gff144a38d-2.1.x86_64.rpm`)
+		checkError(err)
+		// todo: 目录问题
+		err = ioutil.WriteFile(`/tmp/1.rpm`, fish, 0777)
+		checkError(err)
+		err = runCommand(`/usr/bin/yum`, `install`, "-y", `/tmp/1.rpm`)
+		checkError(err)
+
+		fmt.Println("📦 install tmux")
+		tmux, err := f.ReadFile(`rpm/6/tmux-2.9a-4.1.x86_64.rpm`)
+		checkError(err)
+		err = ioutil.WriteFile(`/tmp/2.rpm`, tmux, 0777)
+		checkError(err)
+		err = runCommand(`/usr/bin/yum`, `install`, "-y", `/tmp/2.rpm`)
+		checkError(err)
+	}
 }
 
+// 报错
 func checkError(err error) {
 	if err != nil {
 		panic(err)
 	}
 }
 
+// 获取centos版本
 func getCentosVersion(level string) int {
 	filepath := "/etc/redhat-release"
 	filecontent, err := ioutil.ReadFile((filepath))
@@ -50,4 +119,70 @@ func getCentosVersion(level string) int {
 		ver_num = 0
 	}
 	return ver_num
+}
+
+// 执行cmd命令
+func runCommand(name string, arg ...string) error {
+	// var output []byte
+	// var err error
+	// cmd := exec.Command(name, arg...)
+	// if output, err = cmd.Output(); err != nil {
+	// 	return err
+	// }
+	// fmt.Println(string(output))
+	// return nil
+
+	// cmd := exec.Command(name, arg...)
+	// stdout, err := cmd.StdoutPipe()
+	// if err != nil {
+	// 	return err
+	// }
+	// if err := cmd.Start(); err != nil {
+	// 	return err
+	// }
+	// output_bytes, err := ioutil.ReadAll(stdout)
+	// if err != nil {
+	// 	return err
+	// }
+	// if err := cmd.Wait(); err != nil {
+	// 	return err
+	// }
+	// fmt.Printf("=> \n\n %s", output_bytes)
+	// return nil
+
+	cmd := exec.Command(name, arg...)
+
+	//创建获取命令输出管道
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		fmt.Printf("Error:can not obtain stdout pipe for command:%s\n", err)
+		return err
+	}
+
+	//执行命令
+	if err := cmd.Start(); err != nil {
+		fmt.Println("Error:The command is err,", err)
+		return err
+	}
+
+	//使用带缓冲的读取器
+	outputBuf := bufio.NewReader(stdout)
+	for {
+		//一次获取一行,_ 获取当前行是否被读完
+		output, _, err := outputBuf.ReadLine()
+		if err != nil {
+			// 判断是否到文件的结尾了否则出错
+			if err.Error() != "EOF" {
+				fmt.Printf("Error :%s\n", err)
+				return err
+			}
+			break
+		}
+		fmt.Printf("=> %s\n", string(output))
+	}
+	//wait 方法会一直阻塞到其所属的命令完全运行结束为止
+	if err = cmd.Wait(); err != nil {
+		return err
+	}
+	return nil
 }
